6176cbf7ab14f1d6d9146b6b2738ad8b67d5388a
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-10-04 18:23:11 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,14 +1,14 @@
-import {sticker} from '../src/libraries/sticker.js';
-import uploadFile from '../src/libraries/uploadFile.js';
-import uploadImage from '../src/libraries/uploadImage.js';
-import {webp2png} from '../src/libraries/webp2mp4.js';
+import {sticker} from '../lib/sticker.js';
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import {webp2png} from '../lib/webp2mp4.js';
 
 
 
 const handler = async (m, {conn, args, usedPrefix, command}) => {
   const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.sticker_sticker
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
@@ -17,15 +17,12 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   try {
     const q = m.quoted ? m.quoted : m;
     const mime = (q.msg || q).mimetype || q.mediaType || '';
-    const metadata = {
-      isAiSticker: true
-    }
     if (/webp|image|video/g.test(mime)) {
       const img = await q.download?.();
       if (!img) throw `${tradutor.texto1} ${usedPrefix + command}*`;
       let out;
       try {
-        stiker = await sticker(img, false, global.packname, global.author, ["✨"], metadata);
+        stiker = await sticker(img, false, global.packname, global.author);
       } catch (e) {
         console.error(e);
       } finally {
@@ -34,11 +31,11 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
           else if (/image/g.test(mime)) out = await uploadImage(img);
           else if (/video/g.test(mime)) out = await uploadFile(img);
           if (typeof out !== 'string') out = await uploadImage(img);
-          stiker = await sticker(false, out, global.packname, global.author, ["✨"], metadata);
+          stiker = await sticker(false, out, global.packname, global.author);
         }
       }
     } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author, ["✨"], metadata);
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author);
       else return m.reply(`${tradutor.texto2} ${usedPrefix}s https://telegra.ph/file/0dc687c61410765e98de2.jpg*`);
     }
   } catch (e) {
@@ -61,11 +58,11 @@ const isUrl = (text) => {
 
 
 /* import fetch from 'node-fetch'
-import { addExif } from '../src/libraries/sticker.js'
-import { sticker } from '../src/libraries/sticker.js'
-import uploadFile from '../src/libraries/uploadFile.js'
-import uploadImage from '../src/libraries/uploadImage.js'
-import { webp2png } from '../src/libraries/webp2mp4.js'
+import { addExif } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
 import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
